5f35c6a71b2263dfbfe948028c20c7a6ccc23608
mugichaDX <seijun121210@gmail.com>
2021-11-28 00:08:24 +0900

    first commit

---
+++ test/controllers/hello_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class HelloControllerTest < ActionDispatch::IntegrationTest
+  test "should get #" do
+    get hello_#_url
+    assert_response :success
+  end
+
+  test "should get helloという名前のコントローラーを作成する" do
+    get hello_helloという名前のコントローラーを作成する_url
+    assert_response :success
+  end
+end
